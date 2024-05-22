76f99c1bf40fd4a9d8bdead18d97ea194bfb84d8
ANTHONY <anthony.gutierrez@unl.edu.ec>
2024-05-21 22:18:35 -0500

    Controls
    cursa,periodo,main

---
+++ vista/main.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../')
+ 
+from controlador.cursaDaoControl import CursaDaoControl
+from controlador.periodoDaoControl import PeriodoDaoControl
+
+#pc. = PeriodoDaoControl()
+#ec = EstudianteDaoControl()
+#cc = CursaDaoControl()
+ 
