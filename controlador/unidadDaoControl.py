b51c146775da10b59d6d60aca173fd9e91b974e8
Isaac <isaac.cartuche@unl.edu.ec>
2024-07-22 17:32:32 -0500

    Auto stash before merge of "Isaac" and "origin/main"

---
+++ controlador/unidadDaoControl.py
@@ -0,0 +1,23 @@
+from controlador.dao.daoAdapter import DaoAdapter
+from modelo.unidad import Unidad 
+class UnidadDaoControl(DaoAdapter):
+    def __init__(self,):
+        super().__init__(Unidad)
+        self.__unidad = Unidad()
+        
+    @property
+    def _unidad(self):
+        return self.__unidad
+
+    @_unidad.setter
+    def _unidad(self, value):
+        self.__unidad = value
+
+    @property
+    def _lista(self):
+        return self._list()
+    
+    @property
+    def save(self):
+        self._persona._id = self._lista._length + 1
+        self._save(self._persona)
